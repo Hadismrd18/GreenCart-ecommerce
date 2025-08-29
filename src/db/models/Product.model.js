e6d22d14ca89aa249ac5245eaaccbd1c8409ee64
hadis momeni <hadismraad@gmail.com>
2025-08-29 14:38:58 +0330

    refrence sellerId in the product model

--- src/db/models/Product.model.js
+++ src/db/models/Product.model.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
+  sellerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   name: { type: String, required: true },
   category: { type: String, required: true },
   price: { type: Number, required: true },

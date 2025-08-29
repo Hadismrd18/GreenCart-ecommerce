60152a0704f7b27e7853594b29fb1b5fc588b3ad
hadis momeni <hadismraad@gmail.com>
2025-08-29 14:40:42 +0330

    create api route for adding a  new order

---
+++ src/app/api/orders/route.js
@@ -0,0 +1,38 @@
+// in this route we can add a new order by pressing the order button in the cart
+
+import connectDb from "@/db/connectDb";
+import OrderModel from "@/db/models/Order.model";
+
+export async function POST(request, { params }) {
+  await connectDb();
+  try {
+    const body = await request.json();
+    const {
+      sellerId,
+      items,
+      amount,
+      address,
+      status,
+      paymentType,
+      isPaid,
+      createdAt,
+      updatedAt,
+    } = body;
+
+    const newOrder = OrderModel.create({
+      sellerId,
+      items,
+      amount,
+      address,
+      status,
+      paymentType,
+      isPaid,
+      createdAt,
+      updatedAt,
+    });
+    return new Response(JSON.stringify(newOrder), { status: 201 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error }));
+  }
+}
+// AAAAHHHHHHHHHHHHHHHHHHHH

184ae560241f0480d8eabddfaa03a750744acacd
hadis momeni <hadismraad@gmail.com>
2025-08-29 14:41:07 +0330

    create get api route for getting seller orders in the dashboard

---
+++ src/app/api/orders/[sellerId]/route.js
@@ -0,0 +1,22 @@
+// this route is for getting all the orders on orders page of seller dashboard
+
+import connectDb from "@/db/connectDb";
+import OrderModel from "@/db/models/Order.model";
+import UserModel from "@/db/models/User.model";
+
+export async function GET(req, { params }) {
+  await connectDb();
+  try {
+    const parameters = await params;
+    const sellerId = parameters.sellerId;
+    // check if that id is for a seller
+    const seller = UserModel.find({ _id: sellerId, isSeller: true });
+    if (!seller) {
+      return new Response(JSON.stringify("seller not found"));
+    }
+    const sellerOrders = await OrderModel.find({ _id: sellerId });
+    return new Response(JSON.stringify(sellerOrders), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error }));
+  }
+}

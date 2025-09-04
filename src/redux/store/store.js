551856d259a506a06db3635b824c7b24de353a2d
hadis momeni <hadismraad@gmail.com>
2025-09-04 11:13:28 +0330

    add order reducer

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../slices/user.slice";
-
+import orderReducer from "../slices/order.slice";
 export const makeStore = () => {
-  return configureStore({ reducer: { user: userReducer } });
+  return configureStore({
+    reducer: { user: userReducer, order: orderReducer },
+  });
 };

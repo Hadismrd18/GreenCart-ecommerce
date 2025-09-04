f4f051c418277c627b8e8391aaf022b09e87977f
hadis momeni <hadismraad@gmail.com>
2025-09-04 08:57:30 +0330

    setup redux store

---
+++ src/redux/store/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "../slices/user.slice";
+
+export const makeStore = () => {
+  return configureStore({ reducer: { user: userReducer } });
+};

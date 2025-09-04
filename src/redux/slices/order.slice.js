5ec67fc06ea78bd76dbec03fba4862bcaa76a9a6
hadis momeni <hadismraad@gmail.com>
2025-09-04 11:14:46 +0330

    create order slice

---
+++ src/redux/slices/order.slice.js
@@ -0,0 +1,25 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const FetchOrders = createAsyncThunk("orders/get", async (id) => {
+  try {
+    const res = await fetch(`/api/orders/${id}`);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+const orderSlice = createSlice({
+  name: "order",
+  initialState: {},
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(FetchOrders.fulfilled, (state, action) => {
+      console.log(action.payload);
+    });
+  },
+});
+
+
+export default orderSlice.reducer

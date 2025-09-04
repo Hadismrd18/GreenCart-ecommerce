04f19fa40b0d420218b8817ead6da00a397cadc7
hadis momeni <hadismraad@gmail.com>
2025-09-04 11:14:53 +0330

    create user slice

--- src/redux/slices/user.slice.js
+++ src/redux/slices/user.slice.js
@@ -1,21 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   isLoggedIn: false,
   userData: {},
 };
-
+// based on the user email in session storage make an api call to backend to get userId
+export const findCurrentUser = createAsyncThunk(
+  "userById/get",
+  async (sellerEmail) => {
+    try {
+      const res = await fetch(`/api/users/${sellerEmail}`);
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
     getUser(state, action) {
       const newUser = action.payload;
-      console.log(newUser);
+      state.userData = newUser;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(findCurrentUser.fulfilled, (state, action) => {
+      state.userData = action.payload;
+    });
   },
 });
 
-
-export const {getUser} = userSlice.actions
-export default userSlice.reducer
+export const { getUser } = userSlice.actions;
+export default userSlice.reducer;

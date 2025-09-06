69fb35623b3d288d530ff5e5a3a5b7976da2252a
hadis momeni <hadismraad@gmail.com>
2025-09-06 23:13:58 +0330

    create cart and product reducer

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../slices/user.slice";
 import orderReducer from "../slices/order.slice";
+import cartReducer from "../slices/cart.slice";
+import productSlice from "../slices/product.slice";
 export const makeStore = () => {
   return configureStore({
-    reducer: { user: userReducer, order: orderReducer },
+    reducer: {
+      user: userReducer,
+      order: orderReducer,
+      cart: cartReducer,
+      product: productSlice,
+    },
   });
 };

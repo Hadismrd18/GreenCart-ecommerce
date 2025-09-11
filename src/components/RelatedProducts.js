cd8b17edb50daadc2f6ab903c3dcf7df991525e2
hadis momeni <hadismraad@gmail.com>
2025-09-11 11:12:24 +0330

    connect back and front

--- src/components/RelatedProducts.js
+++ src/components/RelatedProducts.js
@@ -1,9 +1,17 @@
-import React from "react";
+"use client"
+
+import React,{useEffect} from "react";
 import { dummyProducts } from "@/assets/assets";
 import Product from "./Product";
+import { useSelector,useDispatch } from "react-redux";
+import { FetchAllProducts } from "@/redux/slices/product.slice";
 function RelatedProducts({ category }) {
-  console.log(dummyProducts);
-  const currentCategoryItems = dummyProducts.filter((item) => {
+  const allProducts = useSelector((state) => state.product.data);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(FetchAllProducts());
+  }, [dispatch]);
+  const currentCategoryItems = allProducts.filter((item) => {
     return item.category === category;
   });
   console.log(currentCategoryItems);
@@ -14,7 +22,7 @@ function RelatedProducts({ category }) {
           key={index}
           id={item._id}
           offerPrice={item.offerPrice}
-          src={item.image[0].src}
+          src={item.image[0]}
           text={item.name}
           category={item.category}
           price={item.price}

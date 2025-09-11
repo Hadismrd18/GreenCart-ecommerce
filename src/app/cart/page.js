5eb0a3c5e13293176a4a84189fd6e971c3ee07d9
hadis momeni <hadismraad@gmail.com>
2025-09-11 11:13:36 +0330

    minor changes

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -4,11 +4,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { placeOrder, removeItemFromCart } from "@/redux/slices/cart.slice";
 import { useEffect, useState } from "react";
 import seperateOrders from "@/lib/seperateOrders";
+import { useRouter } from "next/navigation";
 export default function page() {
   const [address, setAddress] = useState("this is an address");
   const [deliveryMethod, setDeliveryMethod] = useState("online");
   const [totalPrice, setTotalPrice] = useState(0);
   const cartData = useSelector((state) => state.cart);
+  const router = useRouter();
   console.log(cartData);
   const dispatch = useDispatch();
 
@@ -37,9 +39,10 @@ export default function page() {
         paymentType: deliveryMethod,
         isPaid: true,
       };
-      console.log(orderData)
+      console.log(orderData);
       dispatch(placeOrder(orderData));
     });
+    router.push("/");
   };
 
   return (
@@ -71,12 +74,13 @@ export default function page() {
 
           <div className="space-y-6">
             {/* Item 1 */}
-            {cartData.map((item, index) => (
+            {cartData?.map((item, index) => (
               <div key={index} className="flex items-center gap-4 pb-4">
-                <div
+                <img
+                  src={item.productDetails.data.image[0]}
                   className="w-20 h-20 bg-gray-200 rounded-md flex-shrink-0"
                   aria-hidden="true"
-                ></div>
+                />
 
                 <div className="flex-1">
                   <div className="flex items-center justify-between">
